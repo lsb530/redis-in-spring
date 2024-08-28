7bb777335a24e4a8d9643540b3c94b974dce54ab
boki <lsb530@naver.com>
2024-08-28 14:51:04 +0900

    add redis config and cache manager

--- src/main/java/boki/redisinspring/board/BoardService.java
+++ src/main/java/boki/redisinspring/board/BoardService.java
@@ -1,5 +1,6 @@
 package boki.redisinspring.board;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -9,15 +10,23 @@ import java.util.List;
 @Service
 public class BoardService {
 
-    private BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
 
     public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
+    // @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size' + #size", cacheManager = "boardCacheManager")
+    @Cacheable(cacheNames = "getBoards", key = "T(boki.redisinspring.board.BoardService).createCacheKey(#page, #size)", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         PageRequest pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
         return pageOfBoards.getContent();
     }
+
+    @SuppressWarnings("unused")
+    public static String createCacheKey(int page, int size) {
+        return String.format("boards:page:%d:size:%d", page, size);
+    }
+
 }
